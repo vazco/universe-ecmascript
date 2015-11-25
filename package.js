be4feb7f74873455fdb0dce3176d283e9153021f
cristo-rabani <cristo.rabani@gmail.com>
2015-11-25 21:43:02 +0100

    up

--- package.js
+++ package.js
@@ -1,4 +1,4 @@
-var modulesVersion = '0.6.4'; // this package should release with original universe:modules
+var modulesVersion = '0.6.5'; // this package should release with original universe:modules
 
 Package.describe({
     name: 'universe:ecmascript',
@@ -16,7 +16,6 @@ Package.registerBuildPlugin({
 
 Package.onUse(function (api) {
     api.versionsFrom('1.2.0.2');
-
     // Use Meteor 1.2 build plugin
     api.use(['universe:modules@' + modulesVersion, 'isobuild:compiler-plugin@1.0.0']);
     api.imply('universe:modules@' + modulesVersion);
